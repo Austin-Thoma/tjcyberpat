20324d1735af331795b001ac0c59c5a921d21352
inafi <irfan.k.nafi@gmail.com>
2020-09-20 20:28:38 -0400

    lectures

---
+++ Lectures/Windows10/list.py
@@ -0,0 +1,15 @@
+import glob
+import os
+result = []
+PATH = "/Users/nafi/Develop/GitHub/tjcp/Lectures/Windows10"
+
+for x in os.walk(PATH):
+    files = glob.glob(os.path.join(x[0], '*.pdf'))
+    files.sort(key=os.path.getmtime, reverse=True)
+    for y in files:
+        result.append(y.split("/")[-2] + "/" + y.split("/")[-1])
+
+fout = open("dir.txt", "w")
+
+for i in result:
+    fout.write(i + "\n")
